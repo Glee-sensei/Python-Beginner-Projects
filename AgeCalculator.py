3c9d40999c6ce951ba56c0a0e5514e96433aface
Glee-sensei <jonnieunuane@gmail.com>
2024-03-19 12:37:54 +0100

    age calculator

---
+++ AgeCalculator.py
@@ -0,0 +1,11 @@
+# displays welcome message
+# program ask's user for DOB
+# subtracts user input from current year
+# states user's age with a remark
+# greetings
+
+print("Hello, Human!")
+DOB = input("Type in your year of birth:")
+AGE = (2024-int(DOB))
+print("That's great, you're", AGE, "years old!")
+print("Have a wonderful day!")
